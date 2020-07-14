3e10adda643784482652b262b55856f6e5f1324b
Oleksii Troian <otroian@deloitte.ca>
2020-07-14 09:25:23 -0400

    teat

--- terraform/main.tf
+++ terraform/main.tf
@@ -30,7 +30,6 @@ resource "google_cloud_run_service_iam_policy" "noauth" {
   location = google_cloud_run_service.default.location
   project  = google_cloud_run_service.default.project
   service  = google_cloud_run_service.default.name
-
   policy_data = data.google_iam_policy.noauth.policy_data
 }
 
